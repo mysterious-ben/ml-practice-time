57b04d257fdc1e4eabfeea21b904253654be8cdc
Yaroslav Kopotilov <datascience@tuta.io>
2024-07-28 00:06:03 +0200

    feat: improve gradio chatbot

--- chatbots/chatbot_gradio.py
+++ chatbots/chatbot_gradio.py
@@ -8,4 +8,9 @@ def always_agree(message: str, history: list) -> str:
 # To run: python chatbots/chatbot_gradio.py
 # Docs & examples: https://www.gradio.app/guides/creating-a-chatbot-fast
 if __name__ == "__main__":
-    gr.ChatInterface(always_agree, title="Your Smart AI Assistant").launch()
+    gr.ChatInterface(
+        always_agree,
+        chatbot=gr.Chatbot(show_label=False),
+        title="Your Smart AI Assistant",
+        examples=["What a lovely day!", "I think I'm very handsome.", "Should I grab some coffee?"],
+    ).launch()
